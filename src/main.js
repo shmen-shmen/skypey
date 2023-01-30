df83254d9a4b16ba073dc1517967021ef7ec296d
shmen-shmen <akshevchenko@proton.me>
2023-01-30 19:20:57 +0600

    initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const Main = () => {
+	return (
+		<div className="main">
+			<h2>This is MAIN</h2>
+		</div>
+	);
+};
+
+export default Main;
---
+++ src/sidebar.js
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+	return (
+		<div className="sidebar">
+			<h2>This is SIDEBAR</h2>
+		</div>
+	);
+};
+
+export default Sidebar;
