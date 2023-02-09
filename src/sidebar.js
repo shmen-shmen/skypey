488165bc466f6e5c6de26016aa01dd4c9b69d1f7
shmen-shmen <akshevchenko@proton.me>
2023-02-10 03:33:52 +0600

    sidebar now has STYLE bc who are we without style?

--- src/sidebar.js
+++ src/sidebar.js
@@ -6,7 +6,6 @@ const Sidebar = (props) => {
 	const contacts = props.contacts;
 	return (
 		<aside className="Sidebar">
-			<h2>This is SIDEBAR</h2>
 			{contacts.map((contact) => {
 				return <User user={contact} key={contact["user_id"]} />;
 			})}
@@ -14,19 +13,33 @@ const Sidebar = (props) => {
 	);
 };
 
-const User = (props) => {
-	const { user } = props;
-	console.log(user["profile_pic"]);
+const User = ({ user }) => {
+	const { name, profile_pic, status } = user;
+	const [background, setBackround] = useState("");
 	return (
-		<div className="User">
-			<h3>{user.name}</h3>
+		<div
+			className="User"
+			style={{ backgroundColor: `${background}` }}
+			onMouseOver={() => {
+				setBackround("red");
+			}}
+			onMouseLeave={() => {
+				setBackround("");
+			}}
+		>
 			<div
-				className="user-avatar"
+				className="User-pic"
 				style={{
-					backgroundImage: `url(${user["profile_pic"]})`,
+					backgroundImage: `url(${profile_pic})`,
 				}}
 			/>
-			<div />
+			<div className="User-info">
+				<div className="User-name">
+					<h3>{name}</h3>
+				</div>
+				<div />
+				<span className="User-status">{status}</span>
+			</div>
 		</div>
 	);
 };
